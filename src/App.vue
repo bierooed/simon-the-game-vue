2c37828f9365264bc56ecb0fe9f4319e12069e98
bierooed <hov.hovanes13@gmail.com>
2023-11-19 17:21:47 +0400

    Add difficulty levels, depend on difficulty set highlithing speed & do some other small ui changes

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,45 @@
   <div id="app">
     <h1>Simon the game!</h1>
     <h3>Score - {{ score }}</h3>
+
+    <div>
+      <label
+        >Select a difficulty level - <mark>{{ difficulty }}</mark></label
+      >
+      <br />
+      <label>Highlighting speed - {{ gameSpeed }} ms</label>
+      <div>
+        <button
+          @click="handleGameDifficulty('Easy')"
+          class="difficulty-btn default-btn"
+        >
+          Easy
+        </button>
+        <button
+          @click="handleGameDifficulty('Normal')"
+          class="difficulty-btn default-btn"
+        >
+          Normal
+        </button>
+        <button
+          @click="handleGameDifficulty('Hard')"
+          class="difficulty-btn default-btn"
+        >
+          Hard
+        </button>
+      </div>
+    </div>
     <div id="simon-board">
       <div
         v-for="(color, index) in simonColors"
         :key="index"
         :style="{ backgroundColor: color }"
+        :class="{ highlighted: highlightedButton === index }"
         class="simon-button"
         @click="handleButtonClick(index)"
       ></div>
     </div>
-    <button @click="startGame">Start Game</button>
+    <button class="start-btn default-btn" @click="startGame">Start Game</button>
   </div>
 </template>
 
@@ -21,20 +50,44 @@ export default {
   data() {
     return {
       score: 0,
-      simonColors: ["red", "blue", "green", "yellow"],
+      difficulty: "Easy",
+      gameSpeed: 1500,
+      simonColors: ["crimson", "blue", "green", "purple"],
       gameStarted: false,
       sequence: [],
       playerSequence: [],
+      highlightedButton: null,
     };
   },
 
   methods: {
     startGame() {
+      console.log(document.getElementsByClassName("simon-button"));
       this.gameStarted = true;
       this.sequence = [];
       this.playerSequence = [];
       this.addToSequence();
       this.playSequence();
+    },
+
+    handleGameDifficulty(level) {
+      switch (level) {
+        case "Easy":
+          this.difficulty = "Easy";
+          this.gameSpeed = 1500;
+          break;
+        case "Normal":
+          this.difficulty = "Normal";
+          this.gameSpeed = 1000;
+          break;
+        case "Hard":
+          this.difficulty = "Hard";
+          this.gameSpeed = 400;
+          break;
+        default:
+          this.difficulty = "Easy";
+          this.gameSpeed = 1500;
+      }
     },
 
     addToSequence() {
@@ -55,18 +108,10 @@ export default {
     },
 
     highlightButton(index) {
-      const currentBtn = document.getElementsByClassName("simon-button")[index];
-      const currentColor = currentBtn.style.backgroundColor;
-
-      document.getElementsByClassName("simon-button")[
-        index
-      ].style.backgroundColor = "orange";
-
+      this.highlightedButton = index;
       setTimeout(() => {
-        document.getElementsByClassName("simon-button")[
-          index
-        ].style.backgroundColor = currentColor;
-      }, 1000);
+        this.highlightedButton = null;
+      }, this.gameSpeed);
     },
 
     handleButtonClick(index) {
@@ -115,10 +160,33 @@ export default {
   max-width: 200px;
   margin: 20px auto;
 }
+
+.difficulty-btn {
+  margin: 5px;
+}
+
+.default-btn {
+  padding: 10px 20px;
+  border: 1px solid #ddd;
+  color: #333;
+  background-color: #fff;
+  border-radius: 4px;
+  font-size: 14px;
+  cursor: pointer;
+}
+
+.default-btn:hover {
+  background-color: #d7d7d7;
+}
+
 .simon-button {
   width: 100%;
   padding: 20px;
   font-size: 18px;
   cursor: pointer;
+}
+
+.highlighted {
+  background-color: yellow !important;
 }
 </style>
